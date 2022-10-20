48e20d85b6b013e5c25ec5735880fc57fd2445f6
fiowang <fiona@chicagobooth.edu>
2022-10-20 10:43:14 +0000

    almost doneee

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "application", :action => "homepage" })
+  
+  get("/rock", { :controller => "application", :action => "play_rock" })
 
+  get("/paper", { :controller => "application", :action => "play_paper" })
+
+  get("/scissors", { :controller => "application", :action => "play_scissors" })
 
 end
+
+#ruby: route, controller, action, view
+
+#start with url, define route, controller, define action, html
